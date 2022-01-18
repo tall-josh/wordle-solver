dd8cca6894a320efc987b5622d8576866a0eb4ce
tall-josh <joshua.d.patterson1@gmail.com>
2022-01-18 22:50:03 +1000

    meh

--- README.md
+++ README.md
@@ -7,12 +7,12 @@
 [Wordle](https://www.powerlanguage.co.uk/wordle/) is word puzzle humans seem to like for some reason. Fuck knows why, word puzzles fuck with my brain. So I wrote this to help me look smart on ~~Facebook~~ Meta because we all know looking smart on social media is where it's at!.
 
 1. `poetry run python solve.py`
-2. By default will start with with *note* as the first suggestion. *Note* is a common word that contains common letters.
+2. Select start guess. Start words will never have duplicate chars and are samples according to the frequency of the char in englist. ToDo, determine frequency wrt the word list.
   - You can override it if you wish by typing a 5 letter world and hitting *enter*
 3. Input score `(b)lack, (y)ellow, (g)reen` ie: bbyyg *enter*
-4. The next suggestion will be the most common word after filtering.
+4. The next suggestion will be sampled after filtering.
   - Hit *enter* to accept
-  - Use *!* to select the next most common word, or
+  - Use *!* to select the next word, or
   - select from the list proveded and enter it manually then hit *enter*.
 5. Repeat
 
@@ -24,11 +24,6 @@
 
 `poetry install`
 
-# Limitations
-
-- Only words without duplicate charaters are user
-- Many of the less common words are not accepted by Wordle but if you stick with the common words it seems to be ok
-
 # References
 
-- CSV data from [Rachael Tatman](https://www.kaggle.com/rtatman) on [Kaggle](https://www.kaggle.com/rtatman/english-word-frequency)
+- CSV data from [Rachael Tatman](https://www.kaggle.com/rtatman) on [Kaggle](https://www.kaggle.com/rtatman/english-word-frequency)  **Now used now, I use the list of words supported by wordel. I found them on reddit somewhere**
--- solve.py
+++ solve.py
@@ -2,56 +2,83 @@ import pandas as pd
 
 ALPH = "[abcdefghijklmnopqrstuvwxyz]"
 
-def load_frequ_words():
-    df = pd.read_csv("unigram_freq.csv")
-    df = df[df.word.str.len() == 5]
-    def only_once(s):
-        return len(set(s)) == 5
-    mask = df.word.apply(only_once)
-    df = df[mask].word
-    return df
+def load_words():
+    df = pd.read_csv("words.txt")
+    return df.word
+
+#def load_frequ_words():
+#    df = pd.read_csv("unigram_freq.csv")
+#    df = df[df.word.str.len() == 5]
+#    def only_once(s):
+#        return len(set(s)) == 5
+#    mask = df.word.apply(only_once)
+#    df = df[mask].word
+#    return df
+
+def word_has_n(word, char, n):
+    return len([c for c in word if c == char]) >= n
+
+def word_has_one_of_each(word):
+    return len(set(word)) == len(word)
 
 def solve(df, word=None):
+
     print(f"word: {word}\n---\n")
     _df = df.copy()
     pattern = [ALPH]*5
 
-    sampled_word = _df[_df.str.match("[etainos]"*5)].values[0]
-    _c = 0
+    ooe = _df[_df.apply(word_has_one_of_each)]
+    start_df = ooe[ooe.str.match("[etainos]"*5)]
+    print(start_df)
+    guess_word = start_df.sample(1).values[0]
+
+    resample_idx = 0
     for _ in range(5):
         while True:
-            _sampled_word = input(f"Guess: {sampled_word}: ")
-            if _sampled_word == "!":
-                sampled_word = _df.values[_c]
-                _c += 1
-            elif len(_sampled_word) == 5:
-                sampled_word = _sampled_word
+            _guess_word = input(f"Guess: {guess_word}: ")
+            if _guess_word == "!":
+                guess_word = _df.values[resample_idx]
+                resample_idx += 1
+            elif len(_guess_word) == 5:
+                guess_word = _guess_word
                 break
-            elif _sampled_word == "":
+            elif _guess_word == "":
                 break
             else:
-                sampled_word = _df.sample(1).values[0]
+                guess_word = _df.sample(1).values[0]
 
         contains = []
-        score = input("(b)lack, (g)reen, (y)ellow: ")
-        for i, (guess_char, s) in enumerate(zip(sampled_word, score), 0):
+        colors = input("(b)lack, (g)reen, (y)ellow: ")
+        for i, (guess_char, color) in enumerate(zip(guess_word, colors), 0):
+            color = color.lower()
             try:
-                if s.lower() == "g":
+                if color == "g":
                     pattern[i] = guess_char
-                elif s.lower() == "y":
+                elif color == "y":
                     pattern[i] = pattern[i].replace(guess_char, '')
-                    _df = _df[_df.str.contains(guess_char)]
-                elif s.lower() == "b":
-                    pattern = [p.replace(guess_char, '') for p in pattern]
-            except:
+                    contains += guess_char
+                elif color == "b":
+                    # added if statement to top from removing conirmed chars
+                    # ie:
+                    # TRUST
+                    # ggggb
+                    # without the if would remove the fist t from the pattern
+                    for idx, pat in enumerate(pattern):
+                        if pat != guess_char:
+                            pattern[idx] = pattern[idx].replace(guess_char, "")
+            except Exception as e:
                 breakpoint()
                 print("-")
 
+        for char in set(contains):
+            char_count = len([c for c in contains if c == char])
+            _df = _df[_df.apply(lambda w: word_has_n(w, char, char_count))]
+
         _pattern = "".join(pattern)
         _df = _df[ _df.str.match(_pattern)]
-        sampled_word = _df.values[0]
-        _c = 0
+        guess_word = _df.sample(1).values[0]
+        resample_idx = 0
         print(_df)
 
 
-solve(load_frequ_words())
+solve(load_words())
