404d2b1904135fe288c1a9b2519024aaeefa265c
tall-josh <joshua.d.patterson1@gmail.com>
2022-01-18 12:35:39 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Super Dumb Wordle Solver
+
+1. `poetry run python solve.py`
+2. By default will start with with *note* as the first suggestion. *Note* is a common word that contains common letters.
+  - You can override it if you wish by typing a 5 letter world and hitting *enter*
+3. Input score `(b)lack, (y)ellow, (g)reen` ie: bbyyg *enter*
+4. The next suggestion will be the most common word after filtering.
+  - Hit *enter* to accept
+  - Use *!* to select the next most common word, or
+  - select from the list proveded and enter it manually then hit *enter*.
+5. Repeat
+
+# Requirements
+
+- [PyPoetry](https://python-poetry.org/)
+
+# Insall
+
+`poetry install`
+
+# Limitations
+
+- Only words without duplicate charaters are user
+- Many of the less common words are not accepted by Wordle but if you stick with the common words it seems to be ok
+
+# References
+
+- CSV data from [Rachael Tatman](https://www.kaggle.com/rtatman) on [Kaggle](https://www.kaggle.com/rtatman/english-word-frequency)
---
+++ solve.py
@@ -0,0 +1,86 @@
+import pandas as pd
+
+ALPH = "[abcdefghijklmnopqrstuvwxyz]"
+
+def load_words():
+    with open("words_alpha.txt", 'r') as f:
+        lines = f.readlines()
+
+    lines = set([s.lower().strip() for s in lines if len(s.strip()) == 5])
+    lines = [l for l in lines if len(set(l)) == 5]
+    print(f"num words: {len(lines)}")
+    return pd.Series(list(lines))
+
+def load_frequ_words():
+    df = pd.read_csv("unigram_freq.csv")
+    df = df[df.word.str.len() == 5]
+    def only_once(s):
+        return len(set(s)) == 5
+    mask = df.word.apply(only_once)
+    df = df[mask].word
+    return df
+
+#df[df.str.match("[etainos]"*5)]
+
+def solve(df, word=None):
+    print(f"word: {word}\n---\n")
+    _df = df.copy()
+    pattern = [ALPH]*5
+
+    sampled_word = _df[_df.str.match("[etainos]"*5)].values[0]
+    #sampled_word = _df.sample(1).values[0]
+    _c = 0
+    for _ in range(5):
+        while True:
+            _sampled_word = input(f"Guess: {sampled_word}: ")
+            if _sampled_word == "!":
+                sampled_word = _df.values[_c]
+                _c += 1
+            elif len(_sampled_word) == 5:
+                sampled_word = _sampled_word
+                break
+            elif _sampled_word == "":
+                break
+            else:
+                sampled_word = _df.sample(1).values[0]
+
+
+        contains = []
+        score = input("(b)lack, (g)reen, (y)ellow: ")
+        for i, (guess_char, s) in enumerate(zip(sampled_word, score), 0):
+            try:
+                if s.lower() == "g":
+                    pattern[i] = guess_char
+                elif s.lower() == "y":
+                    pattern[i] = pattern[i].replace(guess_char, '')
+                    _df = _df[_df.str.contains(guess_char)]
+                elif s.lower() == "b":
+                    pattern = [p.replace(guess_char, '') for p in pattern]
+            except:
+                breakpoint()
+                print("-")
+
+        _pattern = "".join(pattern)
+        _df = _df[ _df.str.match(_pattern)]
+        sampled_word = _df.values[0]
+        _c = 0
+
+
+        print(_df)
+        #print(f"guess: {sampled_word}")
+        #print(f"score: {score}")
+        #print("\n")
+
+
+
+#solve(load_words())
+solve(load_frequ_words())
+
+"""
+1. guess random word
+2. input word into wordle
+3. input score 
+4. guess next word
+--> got to 2
+"""
+
